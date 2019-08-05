79b3b024fdfc0e3b2fc3556af67a19d5dfe00091
Marcel Fuhrmann <mfuhrmann@mailbox.org>
2019-08-05 22:09:27 +0200

    some addings

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ The script can be used with PRIS script source to query Foremans API for host in
 * The asset fields CPU, memory, manufacturer, productname, serialnumber, operatingsystem, description are filled with Puppet facts
 * Service binding on interfaces is not possible since Foreman does not support it. So service discovery is required for each `foreignSource`
 
-# PRIS Requisition Configuration
+# Howto 
+
+## PRIS Requisition Configuration
 
 The requisition config is quite simple.
 The variable values will be used in the script.
@@ -29,7 +31,42 @@ source.reqname=REQUISITIONNAME
 mapper = echo
 ```
 
+## OpenNMS 
+
+* As already mentioned, specific service detectors for each requisition could make sense.
+* Also some policies to set categories based on the requisition. For example: PRO, DEV, TEST or similar. So you can use it for filtering in notifications, available views etc.
+* provisiond needs to be configured to get data from pris:
+```
+  <requisition-def import-name="REQUISITIONNAME" import-url-resource="http://PRIS:8000/requisitions/REQUISITIONNAME">
+    <cron-schedule>* */5 * * * ? *</cron-schedule>
+  </requisition-def>
+``` 
+
 # Known issues
 
 If your Foreman uses HTTPS, make sure PRIS JVM is accepting the SSL certificate of the website.
 Check out Ronnys [troubleshooting article](https://opennms.discourse.group/t/troubleshoot-java-with-self-signed-certificates/55).
+
+# Example PRIS output
+
+
+```xml
+<node node-label="hostname.domain.com" foreign-id="422">
+ <interface ip-addr="192.168.142.15" snmp-primary="P"/>
+ <asset name="cpu" value="Intel(R) Xeon(R) Gold 5115 CPU @ 2.40GHz"/>
+ <asset name="ram" value="64.00 GB"/>
+ <asset name="manufacturer" value="Dell Inc."/>
+ <asset name="productname" value="PowerEdge R740xd"/>
+ <asset name="serialnumber" value="326PQQ2"/>
+ <asset name="operatingsystem" value="Ubuntu 16.04 LTS 4.15.18-12-pve"/>
+ <asset name="description" value="Not Specified"/>
+</node>
+```
+
+# Additional code
+
+In my case I'm using host and global parameters in Foreman.
+They are used to provide something like tag informations.
+In OpenNMS it makes sense to create catgories based on this information.
+Since this code is too specific for general use, I've commented it out, but I didn't want to hide it completely.
+If it makes sense for your environment, check out the additional code at the end of the script.
--- foreman.groovy
+++ foreman.groovy
@@ -31,82 +31,100 @@ requisition.setForeignSource(reqname)
 // for each host entry
 for (result in hostlist.results) {
 
-    // fetch correct nodes in hostgroup
-    if (result.ip == null) {
-        logger.info("no IP address set on: " + result.name);
-        continue;
-    }
-    logger.info('Starting with: ' + result.name);
-    // create a new requisition node
-    RequisitionNode requisitionNode = new RequisitionNode()
-    // Set node label and foreign ID for the node
-    requisitionNode.setNodeLabel(result.name)
-    requisitionNode.setForeignId(result.id.toString())
-    // create interface
-    RequisitionInterface requisitionInterface = new RequisitionInterface();
-    requisitionInterface.setIpAddr(result.ip);
-    requisitionInterface.setSnmpPrimary(PrimaryType.PRIMARY);
-    requisitionNode.getInterfaces().add(requisitionInterface);
-    // create facts string and catch data
-    String facturl = urlbase + 'hosts/' + result.id + '/facts' + per_page + '1000'
-    connectionfacts = facturl.toURL().openConnection()
-    connectionfacts.addRequestProperty("Authorization", "Basic ${authentication}")
-    connectionfacts.setRequestMethod("GET")
-    connectionfacts.doOutput = false
-    connectionfacts.connect()
-    def factlist = new JsonSlurper().parseText(connectionfacts.content.text)
-    // create asset list
-    List < RequisitionAsset > assetList = new ArrayList < RequisitionAsset > ()
-    RequisitionAsset assetCpu = new RequisitionAsset()
-    RequisitionAsset assetRam = new RequisitionAsset()
-    RequisitionAsset assetManufacturer = new RequisitionAsset()
-    RequisitionAsset assetProductname = new RequisitionAsset()
-    RequisitionAsset assetSerialnumber = new RequisitionAsset()
-    RequisitionAsset assetOperatingSystem = new RequisitionAsset()
-    RequisitionAsset assetDescription = new RequisitionAsset()
+ // fetch correct nodes in hostgroup
+ if (result.ip == null) {
+  logger.info("no IP address set on: " + result.name);
+  continue;
+ }
+ logger.info('Starting with: ' + result.name);
+ // create a new requisition node
+ RequisitionNode requisitionNode = new RequisitionNode()
+ // Set node label and foreign ID for the node
+ requisitionNode.setNodeLabel(result.name)
+ requisitionNode.setForeignId(result.id.toString())
+ // create interface
+ RequisitionInterface requisitionInterface = new RequisitionInterface();
+ requisitionInterface.setIpAddr(result.ip);
+ requisitionInterface.setSnmpPrimary(PrimaryType.PRIMARY);
+ requisitionNode.getInterfaces().add(requisitionInterface);
+ // create facts string and catch data
+ String facturl = urlbase + 'hosts/' + result.id + '/facts' + per_page + '1000'
+ connectionfacts = facturl.toURL().openConnection()
+ connectionfacts.addRequestProperty("Authorization", "Basic ${authentication}")
+ connectionfacts.setRequestMethod("GET")
+ connectionfacts.doOutput = false
+ connectionfacts.connect()
+ def factlist = new JsonSlurper().parseText(connectionfacts.content.text)
+ // create asset list
+ List < RequisitionAsset > assetList = new ArrayList < RequisitionAsset > ()
+ RequisitionAsset assetCpu = new RequisitionAsset()
+ RequisitionAsset assetRam = new RequisitionAsset()
+ RequisitionAsset assetManufacturer = new RequisitionAsset()
+ RequisitionAsset assetProductname = new RequisitionAsset()
+ RequisitionAsset assetSerialnumber = new RequisitionAsset()
+ RequisitionAsset assetOperatingSystem = new RequisitionAsset()
+ RequisitionAsset assetDescription = new RequisitionAsset()
 
-    // grabbing Puppet facts
-    def stuff = factlist.results.get(result.name)
-    logger.info('Grabbing facts for: ' + result.name);
-    if (stuff == null) {
-        logger.info("no facts for: " + result.name)
-    } else {
-        // define asset and write value
-        assetCpu.setName("cpu")
-        assetCpu.setValue(factlist.results.get(result.name).get("processor0"))
-        assetRam.setName("ram")
-        assetRam.setValue(factlist.results.get(result.name).get("memorysize"))
-        assetManufacturer.setName("manufacturer")
-        assetManufacturer.setValue(factlist.results.get(result.name).get("manufacturer"))
-        assetProductname.setName("productname")
-        assetProductname.setValue(factlist.results.get(result.name).get("productname"))
-        assetSerialnumber.setName("serialnumber")
-        assetSerialnumber.setValue(factlist.results.get(result.name).get("serialnumber"))
-        assetLsbDescription = (factlist.results.get(result.name).get("lsbdistdescription"))
-        assetKernelrelease = (factlist.results.get(result.name).get("kernelrelease"))
-        assetDescription.setName("description")
-        assetDescription.setValue(result.comment)
-        assetOperatingSystem.setName("operatingsystem")
-        assetOperatingSystem.setValue(assetLsbDescription + ' ' + assetKernelrelease)
-        // add asset to list
-        assetList.add(assetCpu)
-        assetList.add(assetRam)
-        assetList.add(assetManufacturer)
-        assetList.add(assetProductname)
-        if (assetSerialnumber.value == null) {
-            logger.info("no serialnumber available: " + result.name);
-            assetSerialnumber.setValue("Not Specified")
-        }
-        assetList.add(assetSerialnumber)
-        assetList.add(assetOperatingSystem)
-        if (assetDescription) {
-            logger.info("no description available: " + result.name);
-            assetDescription.setValue("Not Specified")
-        }
-        assetList.add(assetDescription)
-        requisitionNode.getAssets().addAll(assetList)
+ // grabbing Puppet facts
+ def stuff = factlist.results.get(result.name)
+ logger.info('Grabbing facts for: ' + result.name);
+ if (stuff == null) {
+  logger.info("no facts for: " + result.name)
+ } else {
+  // define asset and write value
+  assetCpu.setName("cpu")
+  assetCpu.setValue(factlist.results.get(result.name).get("processor0"))
+  assetRam.setName("ram")
+  assetRam.setValue(factlist.results.get(result.name).get("memorysize"))
+  assetManufacturer.setName("manufacturer")
+  assetManufacturer.setValue(factlist.results.get(result.name).get("manufacturer"))
+  assetProductname.setName("productname")
+  assetProductname.setValue(factlist.results.get(result.name).get("productname"))
+  assetSerialnumber.setName("serialnumber")
+  assetSerialnumber.setValue(factlist.results.get(result.name).get("serialnumber"))
+  assetLsbDescription = (factlist.results.get(result.name).get("lsbdistdescription"))
+  assetKernelrelease = (factlist.results.get(result.name).get("kernelrelease"))
+  assetDescription.setName("description")
+  assetDescription.setValue(result.comment)
+  assetOperatingSystem.setName("operatingsystem")
+  assetOperatingSystem.setValue(assetLsbDescription + ' ' + assetKernelrelease)
+  // add asset to list
+  assetList.add(assetCpu)
+  assetList.add(assetRam)
+  assetList.add(assetManufacturer)
+  assetList.add(assetProductname)
+  if (assetSerialnumber.value == null) {
+   logger.info("no serialnumber available: " + result.name);
+   assetSerialnumber.setValue("Not Specified")
+  }
+  assetList.add(assetSerialnumber)
+  assetList.add(assetOperatingSystem)
+  if (assetDescription) {
+   logger.info("no description available: " + result.name);
+   assetDescription.setValue("Not Specified")
+  }
+  assetList.add(assetDescription)
+  requisitionNode.getAssets().addAll(assetList)
+ }
 
-    }
+ /*
+        Additional code to grab the values of the
+        global parameters named pconsole_env and pconsole_stack
+        to create OpenNMS categories 
+        
+        String params = urlbase + 'hosts/' + result.id
+        connectionparams = params.toURL().openConnection()
+        connectionparams.addRequestProperty("Authorization", "Basic ${authentication}")
+        connectionparams.setRequestMethod("GET")
+        connectionparams.doOutput = false
+        connectionparams.connect()
+        def paramslist = new JsonSlurper().parseText(connectionparams.content.text)
+
+        def catList = paramslist.all_parameters.findAll{it -> it.name in['pconsole_env', 'pconsole_stack']}.collect{it -> new RequisitionCategory(it.value)};
+        requisitionNode.getCategories().addAll(catList);
+        requisition.getNodes().add(requisitionNode);
+ */
+     }   
 }
 
 return requisition
